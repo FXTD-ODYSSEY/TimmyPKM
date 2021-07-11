a2d12ef215daa1391d1f1ee46164b8a6a61d10fe
timmyliang <timmyliang@tencent.com>
2021-07-12 00:02:09 +0800

    remove watch-fs plugin

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 baseDirForScriptSelf=$(cd "$(dirname "$0")"; pwd)
 export TIDDLYWIKI_PLUGIN_PATH=$baseDirForScriptSelf/Plugins  
 # echo $TIDDLYWIKI_PLUGIN_PATH
-tiddlywiki +plugins/linonetwo/watch-fs IndexWiki --server 8080 
+tiddlywiki IndexWiki --server 8080 
